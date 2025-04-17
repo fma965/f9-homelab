3c227d8b2da4f3625476837ea1ff7bff9172ae4b
Scott <fma96580@gmail.com>
2025-04-17 10:30:07 +0100

    feat(kubernetes): add reflector

--- kubernetes/restore-kubernetes.sh
+++ kubernetes/restore-kubernetes.sh
@@ -36,9 +36,8 @@ NAMESPACE="flux-system"
 kubectl create namespace "$NAMESPACE" --dry-run=client -o yaml | kubectl apply -f -
 
 color_echo "34" "Creating sops-age secret in flux-system namespace from $SOPS_AGE_KEY_FILE ..."
-kubectl create secret generic sops-age --from-file=age.agekey=$SOPS_AGE_KEY_FILE -o yaml --dry-run=client | kubectl apply -f -
-kubectl annotate secret sops-age -n flux-system reflector.v1.k8s.emberstack.com/reflection-allowed="true"
-kubectl annotate secret sops-age -n flux-system reflector.v1.k8s.emberstack.com/reflection-auto-enabled: "true"
+kubectl create secret generic sops-age -n flux-system --from-file=age.agekey=$SOPS_AGE_KEY_FILE -o yaml --dry-run=client | kubectl apply -f -
+kubectl annotate secret sops-age -n flux-system reflector.v1.k8s.emberstack.com/reflection-allowed="true" reflector.v1.k8s.emberstack.com/reflection-auto-enabled="true"
 
 color_echo "34" "Bootstrapping FluxCD ..."
 flux bootstrap github \
@@ -71,7 +70,7 @@ flux suspend kustomization apps
 # BOUNCER_PID=$!
 
 until kubectl wait --for=condition=Ready \
-  certificate/f9-casa -n default \
+  certificate/f9-casa -n cert-manager \
   --timeout=10m 2>/dev/null; do
   color_echo "34" "Waiting for Cert-Manager certificate to be issued..."
   sleep 5
